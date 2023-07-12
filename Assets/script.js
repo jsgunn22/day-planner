73091a4961e9efe7d53e07dfe4dd0a45774a0655
jgunn <j.s.gunn22@gmail.com>
2023-07-12 16:14:02 -0700

    adds input fields to select start and stop time

--- Assets/script.js
+++ Assets/script.js
@@ -53,102 +53,160 @@ $(function () {
     current.year;
   $("#currentDay").text(currentDT);
 
-  // prints time blocks for each hour in the business day
-  for (let h = 9; h < 18; h++) {
-    // change back to 9 & 18
-    // converts military time to standard
-    let hourBlock = h;
-    if (hourBlock > 12) {
-      hourBlock = hourBlock - 12 + "PM";
-    } else if (hourBlock === 12) {
-      hourBlock = hourBlock + "PM";
-    } else {
-      hourBlock = hourBlock + "AM";
-    }
-    let timeAndDate = hourBlock + " | " + fullDate(); // makes localStorage more specic to a day.
-    let getThisBlock = localStorage.getItem(timeAndDate);
+  let startHour = 9;
+  let endHour = 18;
+  let storageStart = localStorage.getItem("startHour");
+  if (storageStart == null) {
+    storageStart = startHour;
+    localStorage.setItem("startHour", startHour);
+  }
+  let storageEnd = localStorage.getItem("endHour");
+  if (storageEnd == null) {
+    storageEnd = endHour;
+    localStorage.setItem("endHour", storageEnd);
+  }
 
-    // prints rows to DOM
-    let hourRow = $("<div>");
-    container.append(hourRow);
-    hourRow.addClass("row time-block"); // remove .past after test
-    let timeBlock = $("<div>");
-    hourRow.append(timeBlock);
-    timeBlock.addClass("col-2 col-md-1 hour text-center py-3");
-    timeBlock.text(hourBlock);
-    let textArea = $("<textarea>");
-    hourRow.append(textArea);
-    textArea.addClass("col-8 col-md-9 description");
-    textArea.attr("rows", "3");
-    let deleteBtn = $("<button>");
-    hourRow.append(deleteBtn);
-    deleteBtn.addClass("delete-btn col-2 col-md-1");
-    deleteBtn.append('<i class="fas fa-trash" aria-hidden="true"></i>');
-    let button = $("<button>");
-    hourRow.append(button);
-    button.addClass("btn saveBtn col-2 col-md-1");
-    button.attr("area-label", "save");
-    button.append('<i class="fas fa-save" aria-hidden="true"></i>');
+  let startInput = $("<input>");
+  $("#inputs").append(startInput);
+  startInput.val(storageStart);
 
-    textArea.text(getThisBlock);
+  let startAmPm = $("<select>");
+  $("#inputs").append(startAmPm);
+  startAmPm.append(
+    "<option value='AM'>AM</option>",
+    "<option value='PM'>PM</option>"
+  );
 
-    if (h < current.hour) {
-      textArea.addClass("past");
-    } else if (h > current.hour) {
-      // prettier-ignore
-      textArea.addClass('future');
-    } else {
-      hourRow.css("z-index", "3");
-      let actualTimePosition = function () {
-        let getTime = new Date();
-        let rowHeight = hourRow.height() / 60;
-        let timeLinePosition = rowHeight * getTime.getMinutes() + "px";
-        return timeLinePosition;
-      };
-      let timeLine = $("<div>");
-      hourRow.append(timeLine);
-      timeLine.addClass("time-line");
-      let currentTimePill = $("<div>");
-      timeLine.append(currentTimePill);
-      currentTimePill.addClass("current-time-pill");
-      let actualTime = function () {
-        let getTime = new Date();
-        let hr = getTime.getHours();
-        let min = getTime.getMinutes();
-        let sec = getTime.getSeconds();
-        let amPm = " AM";
-        if (hr > 12) {
-          hr = hr - 12;
-          amPm = " PM";
-        } else if (hr == 12) {
-          amPm = " PM";
-        }
-        if (min < 10) {
-          min = "0" + min;
-        }
-        if (sec < 10) {
-          sec = "0" + sec;
-        }
-        return hr + ":" + min + ":" + sec + amPm;
-      };
-      currentTimePill.text(actualTime);
-      timeLine.css("top", actualTimePosition);
-      let refresh = setInterval(() => {
+  let endInput = $("<input>");
+  $("#inputs").append(endInput);
+  endInput.val(storageEnd);
+
+  let endAmPm = $("<select>");
+  $("#inputs").append(endAmPm);
+  endAmPm.append(
+    "<option value='AM'>AM</option>",
+    "<option value='PM'>PM</option>"
+  );
+
+  let hoursBtn = $("<button>");
+  $("#inputs").append(hoursBtn);
+  hoursBtn.text("Apply");
+
+  hoursBtn.on("click", function () {
+    let startVal = startInput.val();
+    startHour = startVal;
+    localStorage.setItem("startHour", startVal);
+    let endVal = endInput.val();
+    endHour = endVal;
+    localStorage.setItem("endHour", endVal);
+    container.text("");
+    storageStart = localStorage.getItem("startHour");
+    storageEnd = localStorage.getItem("endHour");
+    renderHours();
+  });
+
+  function renderHours() {
+    let a = parseInt(storageStart); // coverts storage item to num
+    let b = parseInt(storageEnd); // coverts storage item to num
+    // prints time blocks for each hour in the business day
+    for (let h = a; h < b; h++) {
+      // change back to 9 & 18
+      // converts military time to standard
+      let hourBlock = h;
+      if (hourBlock > 12) {
+        hourBlock = hourBlock - 12 + "PM";
+      } else if (hourBlock === 12) {
+        hourBlock = hourBlock + "PM";
+      } else {
+        hourBlock = hourBlock + "AM";
+      }
+      let timeAndDate = hourBlock + " | " + fullDate(); // makes localStorage more specic to a day.
+      let getThisBlock = localStorage.getItem(timeAndDate);
+
+      // prints rows to DOM
+      let hourRow = $("<div>");
+      container.append(hourRow);
+      hourRow.addClass("row time-block"); // remove .past after test
+      let timeBlock = $("<div>");
+      hourRow.append(timeBlock);
+      timeBlock.addClass("col-2 col-md-1 hour text-center py-3");
+      timeBlock.text(hourBlock);
+      let textArea = $("<textarea>");
+      hourRow.append(textArea);
+      textArea.addClass("col-8 col-md-9 description");
+      textArea.attr("rows", "3");
+      let deleteBtn = $("<button>");
+      hourRow.append(deleteBtn);
+      deleteBtn.addClass("delete-btn col-2 col-md-1");
+      deleteBtn.append('<i class="fas fa-trash" aria-hidden="true"></i>');
+      let button = $("<button>");
+      hourRow.append(button);
+      button.addClass("btn saveBtn col-2 col-md-1");
+      button.attr("area-label", "save");
+      button.append('<i class="fas fa-save" aria-hidden="true"></i>');
+
+      textArea.text(getThisBlock);
+
+      if (h < current.hour) {
+        textArea.addClass("past");
+      } else if (h > current.hour) {
+        // prettier-ignore
+        textArea.addClass('future');
+      } else {
+        hourRow.css("z-index", "3");
+        let actualTimePosition = function () {
+          let getTime = new Date();
+          let rowHeight = hourRow.height() / 60;
+          let timeLinePosition = rowHeight * getTime.getMinutes() + "px";
+          return timeLinePosition;
+        };
+        let timeLine = $("<div>");
+        hourRow.append(timeLine);
+        timeLine.addClass("time-line");
+        let currentTimePill = $("<div>");
+        timeLine.append(currentTimePill);
+        currentTimePill.addClass("current-time-pill");
+        let actualTime = function () {
+          let getTime = new Date();
+          let hr = getTime.getHours();
+          let min = getTime.getMinutes();
+          let sec = getTime.getSeconds();
+          let amPm = " AM";
+          if (hr > 12) {
+            hr = hr - 12;
+            amPm = " PM";
+          } else if (hr == 12) {
+            amPm = " PM";
+          }
+          if (min < 10) {
+            min = "0" + min;
+          }
+          if (sec < 10) {
+            sec = "0" + sec;
+          }
+          return hr + ":" + min + ":" + sec + amPm;
+        };
         currentTimePill.text(actualTime);
         timeLine.css("top", actualTimePosition);
-      }, 1000);
-      if (actualTime === "00:00:00") {
-        clearInterval(refresh);
-        location.reload();
+        let refresh = setInterval(() => {
+          currentTimePill.text(actualTime);
+          timeLine.css("top", actualTimePosition);
+        }, 1000);
+        if (actualTime === "00:00:00") {
+          clearInterval(refresh);
+          location.reload();
+        }
       }
-    }
 
-    // does the same as the click listener below
-    // button.on("click", function () {
-    //   let inputText = textArea.val();
-    //   localStorage.setItem(timeAndDate, inputText);
-    // });
+      // does the same as the click listener below
+      // button.on("click", function () {
+      //   let inputText = textArea.val();
+      //   localStorage.setItem(timeAndDate, inputText);
+      // });
+    }
   }
+
+  renderHours();
 
   setInterval(() => {
     let rTime = new Date();
